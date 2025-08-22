4a9040ac5cee9f9b0ffc55097e38f95845d48eee
Albert J. Wong <awong.dev@gmail.com>
2025-08-22 10:23:17 -0700

    Create directories on default storage bucket.

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,23 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export function initializeFirebaseTesting() {
+  if (!processInitialized) {
+    // Initialize test database
+    process.env.GCLOUD_PROJECT = TEST_PROJECT_ID;
+    process.env.FIREBASE_DATABASE_EMULATOR_HOST="127.0.0.1:9000"
+    FirebaseUtils.initializeFirebase({
+        projectId : TEST_PROJECT_ID,
+        databaseURL: `http://127.0.0.1:9000/?ns=${TEST_PROJECT_ID}-default-rtdb`,
+        storageBucket: `${TEST_PROJECT_ID}.appspot.com`,
+    });
+    processInitialized = true;
+  }
+}
+
+if (process.env.SPSBTN_FIREBASE_TESTING === "1") {
+  initializeFirebaseTesting();
+} else {
+  initializeFirebase();
+}
---
+++ setup-sps-by-the-numbers-cache.sh
@@ -0,0 +1,3 @@
+#!bash
+
+gsutil lifecycle set sps-by-the-numbers-cache.json gs://sps-by-the-numbers.appspot.com
