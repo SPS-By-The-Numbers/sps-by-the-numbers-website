c0c1b6c271cef3d738e94fbed0bc56f3f7b289b5
Albert J. Wong <awong.dev@gmail.com>
2025-08-26 22:07:33 -0700

    Remove mdx. Add jest

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 const path = require("path");
 
-/** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
-  },
-})
-
-const nextConfig = {
+module.exports = {
   output: 'export',
   // Append the default value with md extensions
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
@@ -31,6 +20,4 @@ const nextConfig = {
 
     return config;
   }
-}
-
-module.exports = withMDX(nextConfig)
+};
