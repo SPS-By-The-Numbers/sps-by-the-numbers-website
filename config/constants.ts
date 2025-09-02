93b1727a90287fbf6073867e95f77473b51819de
May S. Wong <mswong101@gmail.com>
2025-09-01 19:29:35 -0700

    I added summary and enrollment to Nav bar

--- config/constants.ts
+++ config/constants.ts
@@ -1,4 +1,4 @@
-export const isProduction = process.env.NODE_ENV === 'production';
+export const isProduction = true;
 
 export const SITE_ROOT_URL = 'https://app.sps-by-the-numbers.com';
 export const HOME_URL = 'https://sps-by-the-numbers.com';
