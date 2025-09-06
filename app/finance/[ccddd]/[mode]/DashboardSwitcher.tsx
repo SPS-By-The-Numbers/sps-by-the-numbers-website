139ed8425d327429184e8967f6d75b86c2eb016f
Albert J. Wong <awong.dev@gmail.com>
2025-09-05 21:42:58 -0700

    Give a wait indicator on loading

--- app/finance/[ccddd]/[mode]/DashboardSwitcher.tsx
+++ app/finance/[ccddd]/[mode]/DashboardSwitcher.tsx
@@ -1,12 +1,15 @@
 'use client';
 
-import { useEffect } from 'react';
 import { useDistrictData } from '../../DistrictDataProvider';
+import { useEffect } from 'react';
 import { useHighcharts } from 'components/providers/HighchartsProvider';
-
-import makeSummaryConfig from './SummaryConfig';
 import makeEnrollmentConfig from './EnrollmentConfig';
 import makeExpendituresConfig from './ExpendituresConfig';
+import makeSummaryConfig from './SummaryConfig';
+import Paper from '@mui/material/Paper';
+import LinearProgress from '@mui/material/LinearProgress';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 import "styles/hc-ba-history.scss"
 
@@ -14,6 +17,37 @@ type Params = {
   ccddd: number;
   mode: string;
 };
+
+function getTitle(mode) {
+  switch (mode) {
+    case 'summary':
+        return 'Key District Measures';
+
+    case 'enrollment':
+        return 'Enrollment Details';
+
+    case 'expenditures':
+        return 'Expenditure Analysis';
+  }
+
+  return "[error]";
+}
+
+function updateChart(dashboards, mode, districtData) {
+  // Destroy the last chart if it's there.
+  document.getElementById('dashboard-charts-container').innerHTML = '';
+
+  if (mode === 'summary') {
+    dashboards.board('dashboard-charts-container',
+                     makeSummaryConfig(districtData));
+  } else if (mode === 'enrollment') {
+    dashboards.board('dashboard-charts-container',
+                     makeEnrollmentConfig(districtData));
+  } else if (mode === 'expenditures') {
+    dashboards.board('dashboard-charts-container',
+                     makeExpendituresConfig(districtData));
+  }
+}
 
 export default function DashboardSwitcher({ccddd, mode} : Params) {
   const { highchartsObjs } = useHighcharts();
@@ -29,19 +63,37 @@ export default function DashboardSwitcher({ccddd, mode} : Params) {
         const dashboards = highchartsObjs.dashboards;
         const districtData = districtDataMap[ccddd];
 
-        if (mode === 'summary') {
-          dashboards.board('dashboard-charts-container',
-                           makeSummaryConfig(districtData));
-        } else if (mode === 'enrollment') {
-          dashboards.board('dashboard-charts-container',
-                           makeEnrollmentConfig(districtData));
-        } else if (mode === 'expenditures') {
-          dashboards.board('dashboard-charts-container',
-                           makeExpendituresConfig(districtData));
-        }
+        updateChart(dashboards, mode, districtData);
       }
     },
-    [ccddd, districtDataMap, highchartsObjs]);
+    [ccddd, districtDataMap, highchartsObjs]
+  );
 
-  return (<div id="dashboard-charts-container">Loading...</div>);
+  const title = getTitle(mode);
+
+  return (
+    <Stack component="main" sx={{ margin: "1rem" }}>
+      <Paper sx={{ paddingTop: "0.5rem", paddingBottom: "0.5rem" }}>
+      <Typography component="h1" variant="h1" textAlign="center" style={{fontSize: "2.5rem"}}>{title} Dashboard</Typography>
+      </Paper>
+      <div id="dashboard-charts-container">
+        <LinearProgress />
+          <Paper>
+            <Typography
+              component="h2"
+              variant="h2"
+              textAlign="center"
+              style={{
+                paddingTop: "1rem",
+                paddingBottom: "1rem",
+                fontSize: "1.5rem",
+              }}
+              id="hi"
+            >
+              Loading...
+            </Typography>
+          </Paper>
+      </div>
+    </Stack>
+  );
 }
