85f87395a4dd2774cd2b8c9472746e0bc92246b6
Albert J. Wong <awong.dev@gmail.com>
2025-09-13 21:01:34 -0700

    No dynamic route

--- app/finance/layout.tsx
+++ app/finance/layout.tsx
@@ -1,6 +1,8 @@
 import { fetchDatasetStream } from 'utilities/DistrictData';
 import { parse } from "csv-parse/sync";
 import DistrictDataProvider from './DistrictDataProvider';
+import FinanceNav from 'components/finance/FinanceNav';
+import Paper from '@mui/material/Paper';
 import FinanceNavStateProvider from 'components/providers/FinanceNavStateProvider';
 
 import type { ReactNode } from 'react';
@@ -22,7 +24,10 @@ export default async function FinanceLayout({ children }: {children: ReactNode})
   return (
     <DistrictDataProvider>
       <FinanceNavStateProvider initialCcddd={17001} districts={districts}>
-        {children}
+        <FinanceNav />
+        <Paper sx={{ paddingTop: "0.5rem", paddingBottom: "0.5rem" }}>
+          {children}
+        </Paper>
       </FinanceNavStateProvider>
     </DistrictDataProvider>
   )
